70a0feb9267afb8b40cb486f5578de710c83d514
26TQT01-TiffanyQuex <tiffanyquexdb26@gmail.com>
2026-04-21 10:32:01 -0600

    Add files via upload

---
+++ SolucionGuia3_TiffanyGodoy.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.solucion.guia3_tiffanygodoy;
+
+/**
+ *
+ * @author LAB-D-PC12
+ */
+public class SolucionGuia3_TiffanyGodoy {
+
+    public static void main(String[] args) {
+        jfrmMensajes Eliezer= new jfrmMensajes();
+        Eliezer.setVisible(true);
+    }
+}
